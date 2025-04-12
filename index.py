e0b19d30c9ea37be27589ee5ed4655fdaa432393
whokunalupadhyay <upadhyaykunal2323@gmail.com>
2025-04-12 15:13:46 +0530

    Performed data preprocessing

---
+++ index.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df=pd.read_csv("pollution_data.csv")
+
+# Removing whitespace from column names for ease
+df.columns = df.columns.str.strip()  
+
+        #data summary
+print(df.isnull().sum())
+
+print("Top 5 rows of dataset\n")
+print(df.head())
+
+print("Last 5 rows of dataset\n")
+print(df.tail())
+
+print("Information about the dataset\n")
+print(df.info())
+
+print("Available columns\n")
+print(df.columns.tolist(),"\n")
+
+#checking for duplicates
+duplicates=df.duplicated().sum()
+print(duplicates)
+
+print("Statistical summary\n")
+print(df.describe())
